3d5e87f4f2c47416a0743f99df5506bfd7e00d9a
fzkirablackwhy <nitr5@mail.ru>
2017-10-30 09:20:54 +0300

    was added styles for RecipeDetailsIngredientsList

--- src/components/RecipeDetailsIngredientsList.vue
+++ src/components/RecipeDetailsIngredientsList.vue
@@ -2,8 +2,12 @@
 <div id='ingredientsList'>
 	<div class="ingredients-list">
 		<div v-for="ingredient in ingredients" class="ingredients-list__item">
-			<img :src="ingredient.image" alt="">
-			<p>{{ ingredient.name }}</p>
+			<div class="item__ammount">{{ ingredient.amount }} {{ ingredient.unitShort }}</div>
+			<div class="item__wrapper">
+				<img :src="ingredient.image" alt="">
+			</div>
+
+			<div class="item__title">{{ ingredient.name }}</div>
 		</div>
 	</div>
 </div>
@@ -14,7 +18,8 @@ export default {
 	props: {
 		ingredients: {
 			required: true,
-			type: Array
+			type: Array,
+			default: () => []
 		}
 	}
 }
@@ -22,18 +27,43 @@ export default {
 
 <style lang="sass" scoped>
 .ingredients-list
-    display: flex
+	display: flex
+	flex-wrap: wrap
+	width: 80%
+	margin: auto
+	&__item
+		align-self: center
+		width: 100px
+		height: 100px
+		position: relative
+		margin-right: 5px
+		margin-top: 24px
 
-    &__item
-        align-self: center
+		img
+			max-width: 100px
+			max-height: 69px
+			top: 0
+			bottom: 0
+			left: 0
+			right: 0
+			margin: auto
 
-        img
-            max-width: 100px
-            max-height: 69px
-            vertical-align: middle
-            top: 0
-            bottom: 0
-            left: 0
-            right: 0
-            margin: auto
+.item
+
+	&__wrapper
+		width: 110px
+		height: 105px
+		vertical-align: middle
+		text-align: center
+		line-height: 100px
+		background-color: #fff
+		position: relative
+
+	&__title
+		text-align: center
+		bottom: -15px
+		height: 26px
+		line-height: 14px
+
+
 </style>
